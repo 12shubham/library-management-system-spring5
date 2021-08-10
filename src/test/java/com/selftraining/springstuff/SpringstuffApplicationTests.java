5fb47448389586120e76ba1cd7acdaa573539df4
Shubham Khetan <12shubhamkhetan@gmail.com>
2021-08-10 21:55:57 +0530

    initialized project

---
+++ src/test/java/com/selftraining/springstuff/SpringstuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selftraining.springstuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
